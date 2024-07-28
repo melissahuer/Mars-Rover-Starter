df72df2d55b737dd973ade49eef3c8b921311625
Melissa Huerta <melissahuerta@Melissas-MacBook-Pro.local>
2024-07-27 19:50:51 -0500

    task 1 done

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -8,5 +8,15 @@ describe("Command class", function() {
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
-
+  it("constructor sets command type", function(){
+    const commandType = "MODE";
+    let tstCommand = new Command(commandType);
+    expect(tstCommand.commandType).toBe(commandType);
+  });
+  it("constructor sets a value passed in as the 2nd argument", function(){
+    const commandType = "MOVE"
+    const value = 12000;
+    const tstValue = new Command(commandType, value);
+    expect(tstValue.value).toBe(value);
+  });
 });
