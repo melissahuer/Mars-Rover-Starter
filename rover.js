7e02407a91c903402cd9765a81859cdbeaaacc6e
Melissa Huerta <melissahuerta@Melissas-MBP.lan>
2024-08-03 02:24:57 -0500

    assignment 3 roverjs

--- rover.js
+++ rover.js
@@ -20,42 +20,16 @@ class Rover {
          } else if (command.commandType === 'MOVE') {
             if (this.mode === 'LOW_POWER') {
                results.push({ completed: false })
-               //this.position = 0;
-            } else { 
-               
+            } else {  
                this.position = command.value;
-               results.push({ completed: true })
-
-               
+               results.push({ completed: true })   
             }
          }
          else {
             results.push({ commandType: command.commandType, result: 'unknown' })
          }
-
       }
-      //let results = [];
-      //let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
-      //let message = new Message('Test message with two commands', commands);
-      return { message: message.name, results: results };//: message.name, results: results};
+      return { message: message.name, results: results };
    }
 }
-
-//this.results = results;
-
-
-//unit test 8 draft code
-// let message = new Message("testing message")
-// if(!message){
-//    throw Error ("Message required")
-// }
-//console.log(message);
-// new Message.name;
-
-//    let message = {
-//       testMessage: "test message with two commands"
-//    }
-//    let response = rover.recieveMessage(message);
-//    return response;
-// }
 module.exports = Rover;
